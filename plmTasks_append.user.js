89ac21004e9d8c1a33cabfb1f2d146fb55254c0c
Hugo Delage <hugo.delage@hutchinson.com>
2022-04-08 08:48:14 -0400

    bug ajout plusieurs foix plus ajout jf et zied

--- plmTasks_append.user.js
+++ plmTasks_append.user.js
@@ -1,13 +1,13 @@
 /*globals MY_LIB*/
 // ==UserScript==
-// @name         plmTasks_append
+// @name         plmca_assign
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.2
 // @description  Allow fast edit of project names
 // @author       Hugo Delage
-// @updateURL    http://github.com/hdelage/tamper_hd/raw/main/plmTasks_append.user.js
-// @downloadURL  http://github.com/hdelage/tamper_hd/raw/main/plmTasks_append.user.js
-// @match        https://hut2019x.plm.marquez.ca:444/3dspace/common/*emxPortalDisplay.jsp?*
+// @updateURL    http://github.com/hdelage/tamper_hd/raw/main/plmca_assign.js
+// @downloadURL  http://github.com/hdelage/tamper_hd/raw/main/plmca_assign.js
+// @match        https://*/3dspace/common/emxTableBody.jsp?*portalCmdName=AEFLifecycleApprovals*
 // @icon         https://hut2019x.plm.marquez.ca:444/3dspace/favicon.ico
 // @run-at       document-idle
 // ==/UserScript==
@@ -15,7 +15,38 @@
 /* globals jQuery, $, waitForKeyElements */
 //https://hut2019x.plm.marquez.ca:444/3dspace/common/emxTree.jsp?isPopup=true&mode=tree&objectId=22184.4057.22256.57396&DefaultCategory=PMCSchedule
 
-console.log('HD!!!-------------------- project fast edition init -------------------');
+console.log('HD!!!-------------------- ca fast assign init -------------------');
+
+//$.getJSON("http://github.com/hdelage/tamper_hd/raw/main/plm_roles.json", function(data) { var plm_roles = data; }); // return json file from network for easy modification of assign
+
+const users={"mat1for":"Mathieu F.",
+"hug1del":"Hugo D.",
+"ale1urq":"Alex U.",
+"ran1mac":"Randy",
+"pie1ger":"Pierre G.",
+"A0H95322":"Saleem S.",
+"jea1rat":"J-S. Ratelle",
+"abd1fak":"Abder F.",
+"zoz1olg":"Zozane O.",
+"fra1tre":"Francois T.",
+"mat1leg":"Mathias L.",
+"fre1pot":"Fred P.",
+"nan1fre":"Nancy F.",
+"ala1gau":"Alain G.",
+"ale1ber":"Alex B.",
+"jea1leg":"J-F.",
+"zie1gar":"Zied G."
+}
+
+
+const roles = {
+   "M&P":["mat1for"],
+   "CAD":["hug1del","ale1urq","pie1ger"],
+   "CHARGÉ DE PROJETS":["A0H95322","jea1rat","abd1fak","zoz1olg", "jea1leg"],
+   "METHODE":["fra1tre","mat1leg","fre1pot","nan1fre"],
+   "CAM":["ala1gau"],
+   "PRODUCTION":["ale1ber","zie1gar"]
+}
 
 var wait = (ms) => {
     const start = Date.now();
@@ -25,20 +56,105 @@ var wait = (ms) => {
     }
 }
 
+// taskinfo= id_CA^In Approval^^id_IT^Inbox Task
+// txtAssignee = user (ex. admin_platform)
+// strExist = business object  Inbox Task IT-0013007 1/n    exists = TRUE
+// ITName = IT-00xxxxx
+function url_get_assign(evt){
+    //taskInfo,notificationComment,txtAssignee,ITName
+    var oBt = evt.currentTarget
+    var strExists = "business+object++Inbox Task+" + oBt.ITName + "+1\r\n++++existss+%3D+TRUE"
+    var payload={"taskInfo":encodeURI(oBt.taskInfo),
+                "notificationComment":encodeURI(oBt.notificationComment),
+                "txtAssignee":oBt.txtAssignee,
+                "searchType":"Person",
+               "strExists":strExists}
+    var params = new URLSearchParams(payload).toString()
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "https://hut2019x.plm.marquez.ca:444/3dspace/common/emxLifecycleTaskReassignProcess.jsp?" + params); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+    }
+
+
+function add_button(doc,otable,user, taskInfo,ITname,comment){
+
+    // création des elements
+    var oBt = otable.querySelector('#AssignTo' + user)
+    if (oBt === undefined ) {
+        var oTr = doc.createElement("tr");
+        var oTd = doc.createElement("td");
+        oBt = document.createElement('input');
+
+        const attributes = {
+            id: 'AssignTo' + user ,
+            type: 'button',
+            value: users[user],
+            title: "",
+            class: 'mx_btn-apply',
+        };
+        setAttributes(oBt,attributes)
+
+        //set click events and its parameters
+        oBt.addEventListener ("click", url_get_assign , false);
+        oBt.taskInfo = taskInfo
+        oBt.notificationComment = comment
+        oBt.txtAssignee = user
+        oBt.ITName = ITname
+
+        // finalise append to html
+        oTd.appendChild(oBt);
+        oTr.appendChild(oTd);
+        otable.appendChild(oTr);
+}}
+
+
+function assing_user_buttons(){
+    console.log('-------------------- adding assign user -------------------');
+    //var topdoc = unsafeWindow.top.document
+    var listDisplay = window.open("","listDisplay").document
+    var elems = listDisplay.querySelectorAll('#AEFLifecycleApprovalsSummary > tbody > tr > td > input')
+    if (elems.length > 0) {
+        // table formated
+        // check | State | Assignee | Task/Signature  | Task Title | Comment/Instruction | action | due Dae | completed date
+        // todo  set order dynamic with header
+
+        var tskTitle = undefined
+
+        var assignTable = undefined
+        var type=""
+
+        var taskInfo = ""
+        var ITname = ""
+        var comment = "Réassignation de la tache"
+
+        for (let i = 0; i < elems.length-2; i++) {
+
+
+            tskTitle = elems[0].parentElement.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling
+            if (tskTitle !== undefined) {
+
+                taskInfo = elems[0].value
+                ITname = tskTitle.previousElementSibling.innerText
+                type = tskTitle.textContent.trim()
+                if (roles[type] !== undefined) {
+                    // element exist in json we will add assign buttons here
+                    assignTable = listDisplay.createElement("table")
+                    tskTitle.appendChild(assignTable)
+                    for (let j = 0; j<roles[type].length;j++){
+                        //loop thru users and add button
+                        add_button(listDisplay,assignTable,roles[type][j],taskInfo,ITname,comment)
+}}}}}}
+
 function setAttributes(element, attributes) {
   Object.keys(attributes).forEach(attr => {
     element.setAttribute(attr, attributes[attr]);
   });
 }
 
-
-function check_chanel() {
-  var elem = document.querySelector("#pvChannelTabs > table > tbody > tr > td.tab-active");
-  return elem.html
-}
-
-function addClassNameListener(elemId, callback) {
-    var elem = document.getElementById(elemId);
+// wait for specific class element change
+function addClassNameListener(elemId, odoc,callback) {
+    var elem = odoc.getElementById(elemId);
     var lastClassName = elem.style;
     window.setInterval( function() {
        var className = elem.className;
@@ -50,252 +166,65 @@ function addClassNameListener(elemId, callback) {
 }
 
 
-
-
-function chanelTabListener(elemId, callback) {
-    var lastelem = check_chanel();
-    window.setInterval( function() {
-       var curelem = check_chanel();
-        if (curelem !== lastelem) {
-            callback();
-            lastelem = curelem;
-        }
-    },1000);
-}
-
-function watch_edit_button(callback) {
-    var elem = document.querySelector("#divPvChannel-1-1 > div.pv-channel-content[style=''] > iframe").contentDocument.querySelector('#editButttonId');
-    if (elem !== null) {
-    window.setInterval( function() {
-        if (elem.className === 'icon-button button-active') {
-            callback();
-            wait(100);
-        }
-    },10);
-    };
-}
-
-
-function remove_pattern(){
-     console.log("Retrais du pattern")
-    //var tw = unsafeWindow.window[0];
-    var PMC = unsafeWindow.open("","PMCWBS");
-    var curdoc = PMC.document
-    unsafeWindow.queryTxt = curdoc.querySelector('#HDTaskText').value
-    var emxUICore = unsafeWindow.emxUICore;
-    var oXML = PMC.oXML // get xml data
-    var currentRow = null
-    var rowId = null
-    var aserror = false
-    //nsole.log(PMC)
-    var sel = curdoc.querySelector('#treeBodyTable').querySelectorAll('.mx_rowSelected')
-
-    for(let i in sel) {
-       //document.querySelector('#treeBodyTable').querySelectorAll('.mx_rowSelected')[0].querySelector("[id^=icon_] > a > img").src --> icon image
-       try {
-           if (sel[i].offsetParent === null) {sel = curdoc.querySelector('#treeBodyTable').querySelectorAll('.mx_rowSelected')}
-           remove_element_pattern(sel[i],curdoc, PMC)
-       } catch (e) {
-       console.log(e);
-       wait(1000);
-       remove_pattern;
-           //break;
-       }
-    }
-    wait(1000)
-    toggle_button()
-
-}
-
-function remove_element_pattern(elem,curdoc, PMC){
-    try{
-        var rowTD = elem.querySelector("td.mx_editable")
-        var curtitle = elem.querySelector('td[title]').textContent
-        if (elem.querySelector("[id^=icon_] > a > img").src === 'https://hut2019x.plm.marquez.ca:444/3dspace/common/images/iconSmallTask.png' && curtitle !== "" && curtitle.split("_").length > 1 ) {
-            //rename element
-            //activate element
-            var rowId = elem.getAttribute("id");
-
-            // currentRow = emxUICore.selectSingleNode(oXML, "/mxRoot/rows//r[@id = '" + rowId + "']"); // get all row info
-            PMC.getCell(elem.rowIndex,1);
-            //tw.getcell(x,1)
-            var input = curdoc.querySelector('div.formLayer > input');
-            input.value = curtitle.split("_").slice(0,-1).join("_");
-            wait(50);
-            //document.querySelector("#content").contentDocument.querySelector("#divPvChannel-1-1 > div.pv-channel-content[style=''] > iframe").contentDocument.querySelector('#treeBodyTable').querySelectorAll('.mx_rowSelected > td.mx_editable')[2].click()
-            PMC.updateText(rowTD,input,rowId);
-
-
-    };
-   } catch (e) {
-      console.log(e);
-   }
-}
-
-
-
-
-function apply_pattern(){
-    console.log("Application du pattern")
-//document.querySelector('#treeBodyTable').querySelectorAll('.mx_rowSelected') --> all selected elements
-
-
-    //var tw = unsafeWindow.window[0];
-    var PMC = unsafeWindow.open("","PMCWBS");
-    var curdoc = PMC.document
-    if (curdoc.querySelector('#HDTaskText') !== undefined) {
-        unsafeWindow.queryTxt = curdoc.querySelector('#HDTaskText').value }
-    console.log(curdoc.querySelector('#HDTaskText') )
-    var emxUICore = unsafeWindow.emxUICore;
-    var oXML = PMC.oXML // get xml data
-    var currentRow = null
-    var rowId = null
-    var aserror = false
-
-    //nsole.log(PMC)
-    var sel = curdoc.querySelector('#treeBodyTable').querySelectorAll('.mx_rowSelected')
-
-    for(let i in sel) {
-       //document.querySelector('#treeBodyTable').querySelectorAll('.mx_rowSelected')[0].querySelector("[id^=icon_] > a > img").src --> icon image
-       try {
-           if (sel[i].offsetParent === null) {sel = curdoc.querySelector('#treeBodyTable').querySelectorAll('.mx_rowSelected')}
-           modifyElem(sel[i],unsafeWindow.queryTxt,curdoc, PMC)
-       } catch (e) {
-       console.log(e);
-       wait(10)
-       apply_pattern()
-       //break;
-       }
-    }
-    toggle_button()
-
-}
-
-function modifyElem(elem,hd_txt,curdoc, PMC){
-    var rowTD = elem.querySelector("td.mx_editable")
-    var curtitle = elem.querySelector('td[title]').textContent
-     if (elem.querySelector("[id^=icon_] > a > img").src === 'https://hut2019x.plm.marquez.ca:444/3dspace/common/images/iconSmallTask.png' && curtitle !== "" && curtitle.split("_").slice(-1)[0] !== hd_txt ) {
-        //rename element
-        //activate element
-         var rowId = elem.getAttribute("id");
-
-         // currentRow = emxUICore.selectSingleNode(oXML, "/mxRoot/rows//r[@id = '" + rowId + "']"); // get all row info
-         PMC.getCell(elem.rowIndex,1)
-         //tw.getcell(x,1)
-         var input = curdoc.querySelector('div.formLayer > input')
-         input.value = curtitle + "_" + hd_txt
-         //document.querySelector("#content").contentDocument.querySelector("#divPvChannel-1-1 > div.pv-channel-content[style=''] > iframe").contentDocument.querySelector('#treeBodyTable').querySelectorAll('.mx_rowSelected > td.mx_editable')[2].click()
-         PMC.updateText(rowTD,input,rowId);
-
-    };
-}
-
-
-
-function toggle_button(){
-    var curdoc = document.querySelector("#divPvChannel-1-1 > div.pv-channel-content[style=''] > iframe").contentDocument
-    var hdButton = curdoc.querySelector('#HDTaskButton');
-    var massupdate_bar = curdoc.querySelector("#divMassUpdate > div > div:nth-child(1) > table > tbody > tr")
-
-    if (hdButton === null && massupdate_bar !== null) {
-        // add button,  removed with default plm js?
-
-        // AJOUT SÉPARATEUR
-        var sep_td = document.createElement("td");
-        var set_div = document.createElement("div");
-        set_div.setAttribute('class','separator')
-        sep_td.appendChild(set_div);
-        massupdate_bar.appendChild(sep_td);
-
-
-        // AJOUT BOUTTON pattern
-        var hdButtontd = document.createElement("td");
-        var hdButtonhtml = document.createElement('input'); //'<input id="HDTaskButton" type="button" value="Ajout _pattern" onclick="apply_pattern()" class="mx_btn-apply" title="">')
-        const attributes = {
-            id: 'HDTaskButton',
-            type: 'button',
-            value: 'Ajout _pattern',
-            //onclick: 'apply_pattern()', ---> error not defined use event listener
-            title: "",
-            class: 'mx_btn-apply',
-        };
-        setAttributes(hdButtonhtml,attributes)
-        hdButtonhtml.addEventListener ("click", apply_pattern , false);
-
-        hdButtontd.appendChild(hdButtonhtml);
-        massupdate_bar.appendChild(hdButtontd);
-
-        // ajout du input TXT
-        var intd = document.createElement("td")
-        var inp = document.createElement("input")
-        // get last element after _ in title if not already modified
-        if (unsafeWindow.queryTxt == undefined) {
-            unsafeWindow.queryTxt = curdoc.querySelector(".root-node.even > .mx_editable > div > table > tbody > tr > td[title] > a").text.split("_").slice(-1)[0];
-        }
-        //document.querySelector(".root-node.even > .mx_editable > div > table > tbody > tr > td[title] > a").text.split("_").slice(-1)[0];
-        inp.id = "HDTaskText"
-        inp.value = unsafeWindow.queryTxt
-
-        intd.appendChild(inp);
-        massupdate_bar.appendChild(intd);
-
-        // AJOUT BOUTTON remove pattern
-        var hdButtontd2 = document.createElement("td");
-        var hdButtonhtml2 = document.createElement('input'); //'<input id="HDTaskButton" type="button" value="Ajout _pattern" onclick="apply_pattern()" class="mx_btn-apply" title="">')
-        const attributes2 = {
-            id: 'HDTaskButtonRemove',
-            type: 'button',
-            value: 'Remove',
-            //onclick: 'apply_pattern()', ---> error not defined use event listener
-            title: "",
-            class: 'mx_btn-apply',
-        };
-        setAttributes(hdButtonhtml2,attributes2)
-        hdButtonhtml2.addEventListener ("click", remove_pattern , false);
-
-        hdButtontd2.appendChild(hdButtonhtml2);
-        massupdate_bar.appendChild(hdButtontd2);
-
-
-        //$("#HDTaskButton").click (apply_pattern); //jquery event listerer on click
-        console.log(massupdate_bar)
-        console.log('--------------------ajout du boutton -------------------');
-
-    } else if (hdButton.offsetParent === null) {
-        massupdate_bar.appendChild(hdButtontd);
-        massupdate_bar.appendChild(intd);
-        massupdate_bar.appendChild(hdButtontd2);
-        console.log('--------------------ajout du boutton caché -------------------');
-    } else {
-        console.log('--------------------bouton présent -------------------');
-    }
-
-}
-
+// after launch and everything is loaded
 $(document).ready(function() { //When document has loaded
-    console.log('--------------------project fast edition loaded -------------------');
+    console.log('--------------------ca fast assign loaded -------------------');
     setTimeout(function() {
-        console.log('--------------------spinner start -------------------');
-
-       addClassNameListener('imgProgressDivChannel',function(){
-            console.log('--------------------spinner end -------------------');
-            setTimeout(function() {
-                //var tabs= document.getElementById('pvChannelTabs')
-                //console.log(tabs)
-                watch_edit_button( function(){
-                    console.log('--------------------edit button activated -------------------');
-                    setTimeout(function() {
-                        // active désactive le boutton selons le cas
-
-                        toggle_button()
-
-                     }, 200)
-                });
-                //unsafeWindow.toggleRowGrouping('L0Cancelled');
-                //unsafeWindow.toggleRowGrouping('L0Complete');
-
-            }, 2000)
-        });
-    }, 2000); //1.5 seconds will elapse and Code will execute.
-
+       assing_user_buttons()
+   }, 2000);
 });
+
+/*
+https://www.w3schools.com/js/tryit.asp?filename=tryjs_editor
+
+pour tester le form
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<button id="viewform" >open form</button>
+
+
+
+<script>
+function create_form(){
+    var popup = open("", "Popup", "width=300,height=200");
+
+    var
+
+
+    var aOk = popup.document.createElement("a");
+    aOk.innerHTML = "Click here";
+
+    popup.document.body.appendChild(txtOk);
+    popup.document.body.appendChild(aOk);
+}
+
+var button = document.getElementById("viewform");
+button.addEventListener ("click", create_form , false);
+
+</script>
+
+
+
+</body>
+</html>
+*/
+
+/*
+was a test for added form, but will add button in box instead
+function create_form(group){
+    var oppener = document
+    var popup = open("", "Popup", "width=300,height=200");
+    var txtOk = popup.document.createElement("TEXTAREA");
+    var aOk = popup.document.createElement("a");
+    aOk.innerHTML = "Click here";
+
+    popup.document.body.appendChild(txtOk);
+    popup.document.body.appendChild(aOk);
+
+    // to return value to precedent code: https://stackoverflow.com/questions/13301908/open-new-popup-window-and-return-value
+    //window.opener['dataitem'] = <your return value>;
+    //var somevariable = window['dataitem'];
+}*/
